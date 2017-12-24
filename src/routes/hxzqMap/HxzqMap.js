2d52b38efb46fe23dfb669a10090dc245907905d
zhiqin520 <1534394185@qq.com>
2017-12-24 22:11:10 +0800

    hxzqMap

--- src/routes/hxzqMap/HxzqMap.js
+++ src/routes/hxzqMap/HxzqMap.js
@@ -5,15 +5,21 @@ class HxzqMap extends React.Component {
   render() {
     return (
       <div>
-        <h3 style={{ textAlign: 'center' }}>上海华信证券有限责任公司</h3>
+        <h3 style={{textAlign: 'center'}}>上海华信证券有限责任公司</h3>
         {typeof window !== 'undefined' && (
           <BaiduMap
-            position={{ lng: 121.476267, lat: 31.236398 }}
+            position={{lng: 121.476267, lat: 31.236398}}
             needControl
             height={`${388}px`}
             width={`${690}px`}
           />
         )}
+        <h3 style={{textAlign: 'center'}}>
+          <a href={`http://api.map.baidu.com/marker?location=40.047669,116.313082&title=我的位置&content=百度奎科大厦&output=html`}
+             target='_blank'>
+             查看地图
+          </a>
+        </h3>
       </div>
     );
   }
